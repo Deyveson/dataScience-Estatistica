cca7491cf1c4cd82797f0982c5677d18c4af7637
Deyveson <deyvesonn@outlook.com>
2020-06-15 15:24:17 -0400

    03 - Medidas de tendência central
    Relação entre medidas de tendência central

--- one_project.py
+++ one_project.py
@@ -5,36 +5,46 @@ import scipy
 
 dados = pd.read_csv('dados.csv')
 
-print(sorted(dados['Anos de Estudo'].unique()))
+# print(sorted(dados['Anos de Estudo'].unique()))
+#
+#
+# classes = [dados.Altura.min(), 1.65, 1.75, dados.Altura.max()]
+# labels = ['1 - Baixa', '2 - Média', '3 - Alta']
+#
+# frequencia = pd.value_counts(
+#     pd.cut(
+#         x = dados.Altura,
+#         bins = classes,
+#         labels = labels,
+#         include_lowest = True
+#     )
+# )
+#
+# percentual = pd.value_counts(
+#     pd.cut(
+#         x = dados.Altura,
+#         bins = classes,
+#         labels = labels,
+#         include_lowest = True
+#     ), normalize = True
+# ) * 100
+#
+# dist_freq_altura = pd.DataFrame(
+#     {'Frequência': frequencia, 'Porcentagem (%)': percentual}
+# )
+#
+# dist_freq_altura.rename_axis('Estaturas', axis= 'columns', inplace = True)
+#
+# dist_freq_altura.sort_index(ascending = True, inplace = True)
+#
+# print(dist_freq_altura)
 
+dataset = pd.DataFrame({
+    'Sexo': ['H', 'M', 'M', 'M', 'M', 'H', 'H', 'H', 'M', 'M'],
+    'Idade': [53, 72, 54, 27, 30, 40, 58, 32, 44, 51]
+})
 
-classes = [dados.Altura.min(), 1.65, 1.75, dados.Altura.max()]
-labels = ['1 - Baixa', '2 - Média', '3 - Alta']
+# print(dataset['Idade'].mean())
 
-frequencia = pd.value_counts(
-    pd.cut(
-        x = dados.Altura,
-        bins = classes,
-        labels = labels,
-        include_lowest = True
-    )
-)
+print(dataset.groupby('Sexo').mean().loc['H'])
 
-percentual = pd.value_counts(
-    pd.cut(
-        x = dados.Altura,
-        bins = classes,
-        labels = labels,
-        include_lowest = True
-    ), normalize = True
-) * 100
-
-dist_freq_altura = pd.DataFrame(
-    {'Frequência': frequencia, 'Porcentagem (%)': percentual}
-)
-
-dist_freq_altura.rename_axis('Estaturas', axis= 'columns', inplace = True)
-
-dist_freq_altura.sort_index(ascending = True, inplace = True)
-
-print(dist_freq_altura)
